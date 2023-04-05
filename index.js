cc7f38551424ea1e72956cece5415dc7c626afe6
jeremyxxo14 <jeremyxxo@gmail.com>
2023-04-04 23:29:23 -0400

    funcional(no terminado)

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
+
 const temaOscuro = () => {
-    document.querySelection("body").setAttribute("data-bs-theme", "dark");
-    document.querySelection("#dl-icon").setAttribute("class", "bi bi-sun-fill");
+    document.querySelector("body").setAttribute("data-bs-theme", "dark");
+    document.querySelector("#dl-icon").setAttribute("class", "bi bi-sun-fill");
 
 
 }
 
 const temaClaro = () => {
 
-    document.querySelection("body").setAttribute("data-bs-theme", "light");
-    document.querySelection("#dl-icon").setAttribute("class", "bi bi-moon-fill");
+    document.querySelector("body").setAttribute("data-bs-theme", "light");
+    document.querySelector("#dl-icon").setAttribute("class", "bi bi-moon-fill");
 }
 
 const CambiarTema = () => {
 
-    document.querySelection("body").gettAttribute("data-bs-theme") === "light"?
+    document.querySelector("body").getAttribute("data-bs-theme") === "light"?
     temaOscuro() : temaClaro();
 }
